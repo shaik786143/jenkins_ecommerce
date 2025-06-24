b30df7036772af9a2ff821644139296518d717ae
shaik786143 <shaiksubhani.mb@gmail.com>
2025-06-24 15:19:48 +0530

    fix: add SECRET_KEY setting for tests

--- my_ecommerce/settings.py
+++ my_ecommerce/settings.py
@@ -24,6 +24,8 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-test-key-123456789' if 'test' in sys.argv else os.environ.get('DJANGO_SECRET_KEY')
+
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
